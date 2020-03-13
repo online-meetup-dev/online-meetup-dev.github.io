e397550be6febb50e32f25012072ad721e054920
Atsushi Nakatsugawa <atsushi@moongift.jp>
2020-03-13 12:10:42 +0900

    Support error

--- getData.sh
+++ getData.sh
@@ -1,2 +1,2 @@
-curl -L -o _data/events.json "https://script.google.com/macros/s/AKfycby7374Wq2CLwJaKlzH4xOTHtyvQT-zkv_aDbMNFD5QsOBddICMc/exec?name=meetups";
-curl -L -o _data/count.json "https://script.google.com/macros/s/AKfycby7374Wq2CLwJaKlzH4xOTHtyvQT-zkv_aDbMNFD5QsOBddICMc/exec?name=meetups&action=count";
+curl -f -L -o _data/events.json "https://script.google.com/macros/s/AKfycby7374Wq2CLwJaKlzH4xOTHtyvQT-zkv_aDbMNFD5QsOBddICMc/exec?name=meetups";
+curl -f -L -o _data/count.json "https://script.google.com/macros/s/AKfycby7374Wq2CLwJaKlzH4xOTHtyvQT-zkv_aDbMNFD5QsOBddICMc/exec?name=meetups&action=count";
